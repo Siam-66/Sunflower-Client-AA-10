cae50d27a398ecf4896e072a1688e13d92645d83
Siam-66 <siammahmud66@gmail.com>
2024-12-08 02:12:23 +0600

    environment variable

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyDAWpM0g3n8WMxFKFYxMnfqEhMlQuXbZRo",
-authDomain: "sunflower66-57b4d.firebaseapp.com",
-projectId: "sunflower66-57b4d",
-storageBucket: "sunflower66-57b4d.firebasestorage.app",
-messagingSenderId: "711325697245",
-appId: "1:711325697245:web:a192c35f1491a70d004734"
+apiKey: import.meta.env.VITE_apiKey,
+authDomain: import.meta.env.VITE_authDomain,
+projectId: import.meta.env.VITE_projectId,
+storageBucket: import.meta.env.VITE_storageBucket,
+messagingSenderId: import.meta.env.VITE_messagingSenderId,
+appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
