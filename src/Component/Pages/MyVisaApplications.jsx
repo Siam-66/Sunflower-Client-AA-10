4f589d53fc50e8cc58bf499083617f47e8bb867f
Siam-66 <siammahmud66@gmail.com>
2024-12-08 22:36:19 +0600

    done?.notdone?

--- src/Component/Pages/MyVisaApplications.jsx
+++ src/Component/Pages/MyVisaApplications.jsx
@@ -1,8 +1,14 @@
-
+import { Link } from "react-router-dom";
 const MyVisaApplications = () => {
     return (
         <div>
-            My Visa applications
+            <h1 className="text-center text-3xl bg-gradient-to-r from-yellow-900 via-yellow-400 to-yellow-900 bg-clip-text text-transparent font-bold">
+                My Visa applications
+            </h1>
+            <p className="text-center m-20 text-5xl font-bold">
+                No data available 
+            </p>
+            
         </div>
     );
 };
